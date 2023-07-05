865f6b15dbfe1a1073e18ef6c4ab0b9fa25ab0ec
Quentin <orsini.quentin@gmail.com>
2023-07-05 16:20:26 +0200

    add urls

--- urls.js
+++ urls.js
@@ -1,7 +1,7 @@
 // Insert the backend URL given by Vercel in this variable
-const BACKEND_URL = '';
+const BACKEND_URL = "https://my-moviz-back-lime.vercel.app";
 
 // Insert the frontend URL given by Vercel in this variable
-const FRONTEND_URL = '';
+const FRONTEND_URL = "https://my-moviz-front-nu.vercel.app";
 
 module.exports = { BACKEND_URL, FRONTEND_URL }; // Do not edit/remove this line
